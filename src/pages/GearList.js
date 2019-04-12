612ec351f248a5f77c7fed095cc5769ee96c2ca2
clintsearl <clintsearl@gmail.com>
2019-04-12 13:31:12 -0600

    added mdbreact table

--- src/pages/GearList.js
+++ src/pages/GearList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 // import {Link} from 'react-router-dom'
+import { MDBCard, MDBCardBody, MDBCardHeader, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBTableFoot, MDBIcon  } from 'mdbreact';
+
 
 
 
@@ -17,28 +19,33 @@ class GearList extends Component {
   
  componentDidMount(){
   fetch('http://localhost:4001/')
-  // const loadGear = async()=>{
-  //  await fetch("http://api.randomuser.me")
-  //  const data = await results.json()
-  //  this.setState({ gear: data.results[0]})
-    // fetch('https://api.coinmarketcap.com/v1/ticker/?limit=10')
-  // fetch('https://raw.githubusercontent.com/CivilServiceUSA/us-senate/master/us-senate/data/us-senate.json')
-    //this turns the returned data in JSON
     .then(result =>  {
       return result.json();
     })
 
     .then(result => {
       this.setState({gear: result})
-    });
+    });}
+   
   
-    
-  }
-
-
-
 
   render() {
+
+   const deleteItem = async (e)=>{
+     debugger
+      const data = JSON.stringify(this.state._id)
+      await fetch("http://localhost:4001/",{
+        method:"DELETE",
+        body: data,
+        headers:{
+          "Content-Type": "application/json"
+        }
+      }
+  
+  
+      )}
+      
+    
     console.log(this.state)
     return (
       
@@ -52,7 +59,40 @@ class GearList extends Component {
           )
             )}
         </div>
+            <MDBTable responsive striped={`true`} bordered>
+      <MDBTableHead>
+        <tr>
+          <th>Gear Name</th>
+          <th>Brand</th>
+          <th>Weight</th>
+          <th>Catagory</th>
+          <th>Edit / Delete</th>
+        </tr>
+      </MDBTableHead>
+      <MDBTableBody>
+        {this.state.gear.map((gear, index)=>(
+        <tr key={index} striped>
+          <td>{gear._id}</td>
+          <td>{gear.brand}</td>
+          <td>{gear.weight}</td>
+          <td>{gear.category}</td>
+          <td><MDBIcon icon="edit" onClick={e =>deleteItem(gear._id)} /><MDBIcon icon="trash-alt" className="ml-4" value={gear._id} onClick={e=>deleteItem(this.state.gear._id)}/></td>
+        </tr>
+        ))}
+      </MDBTableBody>
+      <MDBTableFoot>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Total</td>
+          <td></td>
+
+        </tr>
+      </MDBTableFoot>
+    </MDBTable>
       </div>
+
+
     );
   }
 }
