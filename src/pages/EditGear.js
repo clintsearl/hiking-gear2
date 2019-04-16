58902cd2e10b8a6b5800c2a23f1613c7fd4a7fdb
clintsearl <clintsearl@gmail.com>
2019-04-16 17:15:47 -0600

    modal showing up with data in it not editing

---
+++ src/pages/EditGear.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+//!!!!!!
+// 
+//      Current plan, this is a copy of the add gear page I want to route to it
+//     then from here make the GET, and then the data placeholders. let them 
+//          change it, and then do the PUT  and redirect to the GearList
+// !!!!!
+
+const AllPage= styled.main`{
+  margin-left:auto;
+  max-width:70%;
+}`
+
+
+
+class AddGear extends Component {  
+  // constructor(props){
+  //   super(props)
+    state ={
+     
+      name:'',
+      brand: '',
+      weight: 0,
+      category:'',
+      units: ''
+    // }
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]: e.target.value}
+      , console.log(e.target.value))
+  }
+  handleClick(e){
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value})
+    
+  }
+
+handleSubmit= async (e)=>{
+ e.preventDefault()
+
+ const data = JSON.stringify(this.state)
+ console.log("first", data)
+//  debugger
+  await fetch("http://localhost:4001",{
+    method:"POST",
+    body: data,
+    headers:{
+      "Content-Type": "application/json"
+    }
+   
+
+  },console.log("second", data)).then(response => console.log(response.json()))
+    
+  }
+
+  render() {
+    
+    console.log(this.state)
+    
+    return (
+      <AllPage>
+        <form onSubmit= {this.handleSubmit}>
+            <div className="form-row">
+                <div className="form-group col-md-4">
+                    <label HTMLfor="gearName">Gear Item</label>
+                    <input type="text" className="form-control" name="name" placeholder="What do you call it?" value={this.state.name} onChange={this.handleChange} />
+                </div>
+                <div className="form-group col-md-4">
+                    <label HTMLfor="brand">Brand</label>
+      <input type="text" className="form-control" name="brand" placeholder="Brand" value={this.state.brand} onChange={this.handleChange} />
+    </div>
+  </div>
+  <div className="form row">
+  <div className="form-group col-md-3">
+    <label HTMLfor="weight">Weight</label>
+    <input type="number" step="0.01" className="form-control" name="weight" placeholder="Select the Units" value={this.state.value} onChange={this.handleChange}/>
+    </div>
+    <div className="form-group col-md-5">
+      <label HTMLfor="category">Category</label>
+      <select name="category" className="form-control" onChange={this.handleChange}>
+        <option defaultValue="uncatagorized">Choose...</option>
+        <option value="essentials">Essentials</option>
+        <option value="sleep">Sleep</option>
+        <option value="kitchen">Kitchen</option>
+        <option value="clothing">Clothing</option>
+        <option value="personal">Personal</option>
+        <option value="emergency">Emergency</option>
+      </select>
+    </div>
+
+    </div>
+    <div className="form-group col-md-2">
+    <div className="btn-group mr-2" role="group" aria-label="First group">
+    {/* <button type="button" className="btn btn-secondary" value= "lb" onClick={e =>this.setState({units: e.target.value})} name="units">lb</button> */}
+    <button type="button" className="btn btn-secondary" value= "oz" onClick={this.handleChange} name="units">oz</button>
+    <button type="button" className="btn btn-secondary" value= "g" onClick={this.handleChange} name="units">g</button> 
+    </div> 
+  
+  
+ 
+     
+  </div>
+  <button type="submit" className="btn btn-primary" style={{justifyContent:'center'}}>Add Item</button>
+</form>
+      </AllPage>
+    );
+  }
+}
+
+export default AddGear;
--- src/pages/GearList.js
+++ src/pages/GearList.js
@@ -1,59 +1,106 @@
-import React, { Component } from 'react';
-// import {Link} from 'react-router-dom'
-import { MDBCard, MDBCardBody, MDBCardHeader, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBTableFoot, MDBIcon  } from 'mdbreact';
+import React, { Component, useState} from 'react';
+// import {BrowserRouter, Link} from 'react-router-dom'
+import { MDBCard, MDBCardHeader, MDBBtn, MDBTable, MDBTableBody, MDBTableHead, MDBTableFoot, MDBIcon, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBModal, MDBContainer } from 'mdbreact';
+// import Modal from '../components/Modal'
+import styled from 'styled-components'
 
 
-
+const allpage = styled.main`{
+}`
 
 
 
 class GearList extends Component {
   //State is what the data is presently
-  state ={
-      gear:[]
-      // name:'',
-      // brand: '',
-      // weight: 0,
-      // catagory:'',
-      // units: ''
-    }
+  state = {
+    gear: [],
+    modal: false,
+    // editGear: {},
+    editGearItem:{}
+    // name:'',
+    // brand: '',
+    // weight: 0,
+    // catagory:'',
+    // units: ''
+  }
   //Props are the properties themselves 
   //constructor and state building the bookshelf
   //go and fetch the books
- componentDidMount(){
-  fetch('http://localhost:4001/')
-    .then(result =>  {
-      return result.json();
-    })
+  componentDidMount() {
+    fetch('http://localhost:4001/')
+      .then(result => {
+        return result.json();
+      })
+      .then(result => {
+        this.setState({ gear: result })
+      });
+  }
 
-    .then(result => {
-      this.setState({gear: result})
-    });}
-   
-  //  deleteItem =  async (e)=>{
-    
-  //     //  debugger
-  //     const data = JSON.stringify(this.state._id)
-  //       await fetch("http://localhost:4001/",{
-  //         method:"DELETE",
-  //         body: data,
-  //         headers:{
-  //           "Content-Type": "application/json"
-  //         }
-  //       }
-    
-    
-  //       )}
+  toggle = async (e) => {
+    await fetch(`http://localhost:4001/${e}`)
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        this.setState({ editGearItem:data})
+      })
+      .then(
+        this.setState({
+          modal: !this.state.modal
+        }))
+    console.log("toggle", this.state.editGearItem)
+
+  }
+
+  editItem = async (e) => {
+    // e.preventDefault()
+    // console.log("all", this.state)
+    // let data = 
+    const data = JSON.stringify(this.state)
+    await fetch(`http://localhost:4001/gearlist/:${data._id}`, {
+      method: "PUT",
+      body: data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    ).then(
+      this.setState({
+        modal: !this.state.modal
+      }))
+  }
+
+  deleteItem = async (e) => {
+    // console.log("here",e)
+    //  debugger
+    let data = { _id: e }
+    data = JSON.stringify(data)
+    await fetch("http://localhost:4001/gearlist/", {
+      method: "DELETE",
+      body: data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    )
+    // .then.props.push(Link ="/gearlist")
+  }
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]: e.target.value}
+      , console.log(e.target.value))
+  }
 
   render() {
 
-   
-      
-    
-    console.log(this.state)
+
+    const { editGearItem } = this.state
+
+    console.log(this.state.editGear)
     return (
-      
-      <div className="GearList">
+
+      <div className="allpage">
+      {/* <MDBContainer> */}
         <h1>List of all your stuff</h1>
         {/* <div>
           {this.state.gear.map((gear, index)=>(
@@ -63,41 +110,71 @@ class GearList extends Component {
           )
             )}
         </div> */}
-            <MDBTable responsive striped color= 'black'>
-      <MDBTableHead  color= 'black'>
-        <tr>
-          <th>Gear Name</th>
-          <th>Brand</th>
-          <th>Weight</th>
-          <th>Catagory</th>
-          <th>Edit / Delete</th>
-        </tr>
-      </MDBTableHead>
-      <MDBTableBody>
-        {this.state.gear.map((gear, index)=>(
-        <tr key={index}>
-          <td>{gear.name}</td>
-          <td>{gear.brand}</td>
-          <td>{gear.weight}</td>
-          <td>{gear.category}</td>
-          <td><MDBIcon icon="edit"/><MDBIcon icon="trash-alt" className="ml-4" key={index} 
-          value={gear._id} 
-          // onClick={this.deleteItem(this.value)}
-          /></td>
-        </tr>
-        ))}
-      </MDBTableBody>
-      <MDBTableFoot>
-        <tr>
-          <td></td>
-          <td></td>
-          <td>Total</td>
-          <td></td>
+        
+        <MDBTable responsive striped color='black'>
+          <MDBTableHead color='black'>
+            <tr>
+              <th>Gear Name</th>
+              <th>Brand</th>
+              <th>Weight</th>
+              <th>Catagory</th>
+              <th>Edit / Delete</th>
+            </tr>
+          </MDBTableHead>
+          <MDBTableBody>
+            {this.state.gear.map((gear, index) => (
+              <tr key={index}>
+                <td>{gear._id}</td>
+                <td>{gear.brand}</td>
+                <td>{gear.weight}</td>
+                <td>{gear.category}</td>
+                <td>
+                  <button className="btn btn-outline-secondary waves-effect" value={gear._id} 
+                  // onClick={(e) => this.toggle(e.target.value)}
+                  onClick={lin}
+                  >Edit</button>
+                  {/* <MDBIcon icon="edit"/> */}
 
-        </tr>
-      </MDBTableFoot>
-    </MDBTable>
-      </div>
+                  <button className="btn btn-outline-primary waves-effect" value={gear._id}
+                    onClick={(e) => this.deleteItem(e.target.value)}>
+                    <MDBIcon icon="trash-alt" className="ml-4" aria-hidden="true" />
+                    {/* </MDBBtn> */}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </MDBTableBody>
+          <MDBTableFoot>
+            <tr>
+              <td></td>
+              <td></td>
+              <td>Total</td>
+              <td></td>
+
+            </tr>
+          </MDBTableFoot>
+        </MDBTable>
+        
+            {/* <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
+              <MDBModalHeader toggle={this.toggle}>MDBModal title</MDBModalHeader>
+              <form onSubmit={(e)=>this.editItem(e.target)}>
+              <MDBModalBody>
+                <p>Edit your gear</p>
+              
+                <input type="text" name="name" placeholder={this.state.editGearItem.name} onChange={this.handleChange}></input>
+
+              </MDBModalBody>
+              <MDBModalFooter>
+                <MDBBtn color="secondary" >Close</MDBBtn>
+                <button color="primary" type="submit">Save changes</button>
+
+              </MDBModalFooter>
+                  </form>
+            </MDBModal> */}
+            {/* </MDBContainer> */}
+
+            
+       </div>
 
 
     );
