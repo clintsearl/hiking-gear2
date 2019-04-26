b47d2c496aa2fcd9cf1a8559504c9a5d22076bbe
clintsearl <clintsearl@gmail.com>
2019-04-26 09:38:09 -0600

    css welcome page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Navigation from './components/navigation'
 import Routing from './components/Route'
 import Welcome from './pages/Welcome';
 // import {Link} from 'react-router-dom'
-//stuff is changing! test branch for NeDB
-
+//colors for the whole place https://www.design-seeds.com/slow-lifestyle/pineapple-tones/
+// #B1BBBB
+// #737E7D
+// #393E43
+// #202124
+// #3E3938
+// #AD9477
 
 class App extends Component {
   render() {
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,38 +3,64 @@ import styled from 'styled-components'
 import staticImg from '../components/assets/pano-lake.jpg'
 
 const Allpage= styled.div`
-background:
+background-color:#202124;
+hight:100vh;
+width:100vw;
+display: flex;
+flex-direction:column;
+
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
 `
 
 const Background =styled.section`
     text-align: center;
     background-image: url(${staticImg}); 
     height: 50vh;
+    width:100vw;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover; 
+`
+const Message = styled.main`
+padding:5%;
+
+
+
 ` 
 const LandingImg = styled.img`
         background-color: #282c34;
-        max-width:100vw;
-        
-        display: flex;
-        flex-direction: column;
+        max-width:100vw;  
         align-items: center;
         
+`
+const Footer = styled.footer`
+  background-color:#AD9477;
+  width:100vw;
+  position:absolute;
+ bottom:0;
 `
 const Welcome =()=> {
     return(
       <Allpage>  
             {/* <LandingImg src={staticImg}/> */}
-          <Background>
+          <Background>stay in front!!!
           </Background>
                 
-             
+          <Message>
             <h1>Welcome to Backpackers database</h1>
             
             <p>“It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing 
             where you might be swept off to.”</p> 
+            </Message>
+            
+            <Footer>
+              Clint Earl
+              link
+              link
+            </Footer>
         </Allpage>
         
     )
